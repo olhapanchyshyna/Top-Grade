a4840a6cf878a373e0df054243225871409de18e
Olya <olapancisina1@gmail.com>
2024-07-28 09:18:02 +0200

    fix fonts 1.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-import { Manrope } from 'next/font/google'
+import { Manrope } from "next/font/google";
 import './globals.css'
 
 const manrope = Manrope({ subsets: ['latin'] })
